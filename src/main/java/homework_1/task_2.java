ba69c7234a4a32bdb97dda7895c506c67ba7240e
Boris <BorerKicker@yandex.ru>
2022-10-20 07:37:17 +0300

    added task_2

--- src/main/java/homework_1/task_2.java
+++ src/main/java/homework_1/task_2.java
@@ -1,2 +1,19 @@
-package homework_1;public class task_2 {
+package homework_1;
+
+public class task_2 {
+    public static void main(String[] args){
+        int number = 1000;
+        for(int i = 1; i <= number; i++){
+            int count = 0;
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0){
+                    count++;
+                }
+            }
+            if(count == 2){
+                System.out.print(i + " ");
+
+            }
+        }
+    }
 }
