19e5ff2091db15c2b5e38adf8a1e68c11ba7c047
Boris <BorerKicker@yandex.ru>
2022-10-20 07:43:33 +0300

    added 6 line

--- src/main/java/homework_1/task_2.java
+++ src/main/java/homework_1/task_2.java
@@ -3,6 +3,7 @@ package homework_1;
 public class task_2 {
     public static void main(String[] args){
         int number = 1000;
+        System.out.print("Simple numbers: ");
         for(int i = 1; i <= number; i++){
             int count = 0;
             for(int j = 1; j <= i; j++){
