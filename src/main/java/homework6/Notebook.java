05fd448409da3c9660e8167355746098e475e3e2
Boris <BorerKicker@yandex.ru>
2022-11-24 17:36:04 +0300

    added a description of the class laptop

--- src/main/java/homework6/Notebook.java
+++ src/main/java/homework6/Notebook.java
@@ -1,8 +1,111 @@
 package homework6;
 
-public class NotebookСomputer {
-  private String nameNotebook;
-  private String seri
-  private int ram;
-  private int volumeHdd;
+import java.util.Objects;
+
+public class Notebook {
+  private String model;
+  private String productSeries;
+  private int id;
+  private String ram;
+  private String volumeHdd;
+  private String operatingSystem;
+  private String colorNotebook;
+
+  private double price;
+  @Override
+  public String toString() {
+    return "Notebook{" +
+        "model='" + model + '\'' +
+        ", productSeries='" + productSeries + '\'' +
+        ", id=" + id +
+        ", ram='" + ram + '\'' +
+        ", volumeHdd='" + volumeHdd + '\'' +
+        ", operatingSystem='" + operatingSystem + '\'' +
+        ", colorNotebook='" + colorNotebook + '\'' +
+        ", price=" + price +
+        '}';
+  }
+
+
+
+  public double getPrice(){
+    return price;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public String getProductSeries() {
+    return productSeries;
+  }
+
+  public String getRam() {
+    return ram;
+  }
+
+  public int getId() {
+    return id;
+  }
+  public String getVolumeHdd() {
+    return volumeHdd;
+  }
+
+  public String getOperatingSystem() {
+    return operatingSystem;
+  }
+
+  public String getColorNotebook() {
+    return colorNotebook;
+  }
+
+  public void setPrice(double price){
+    this.price = price;
+  }
+  public void setModel(String model) {
+    this.model = model;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public void setProductSeries(String productSeries) {
+    this.productSeries = productSeries;
+  }
+
+  public void setRam(String ram) {
+    this.ram = ram;
+  }
+
+  public void setVolumeHdd(String volumeHdd) {
+    this.volumeHdd = volumeHdd;
+  }
+
+  public void setOperatingSystem(String operatingSystem) {
+    this.operatingSystem = operatingSystem;
+  }
+
+  public void setColorNotebook(String colorNotebook) {
+    this.colorNotebook = colorNotebook;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Notebook notebook = (Notebook) o;
+    return id == notebook.id && ram.equals(notebook.ram) && volumeHdd.equals(notebook.volumeHdd)
+        && model.equals(notebook.model) && productSeries.equals(notebook.productSeries)
+        && operatingSystem.equals(notebook.operatingSystem) && colorNotebook.equals(
+        notebook.colorNotebook);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(model, productSeries, ram, volumeHdd,
+        operatingSystem, colorNotebook, 27);
+  }
 }
--- src/main/java/homework6/task1.java
+++ src/main/java/homework6/task1.java
@@ -1,2 +1,84 @@
-package homework6;public class task1 {
+package homework6;
+
+// Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы.
+// Реализовать в java.
+// Создать множество ноутбуков.
+// Написать метод, который будет запрашивать у пользователя критерий (или критерии)
+// фильтрации и выведет ноутбуки, отвечающие фильтру. Критерии фильтрации можно хранить
+// в Map. Например: “Введите цифру, соответствующую необходимому критерию:
+// 1 - ОЗУ
+// 2 - Объем ЖД
+// 3 - Операционная система
+// 4 - Цвет …
+// 3.1. Далее нужно запросить минимальные значения для указанных критериев - сохранить
+// параметры фильтрации можно также в Map.
+// 4. Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class task1 {
+
+  public static void main(String[] args) {
+    Notebook notebook1 = new Notebook();
+    notebook1.setModel("Huawei MateBook D 14");
+    notebook1.setId(1);
+    notebook1.setProductSeries("53013erk");
+    notebook1.setOperatingSystem("Windows 11 Home");
+    notebook1.setRam("8 ГБ");
+    notebook1.setVolumeHdd("256 ГБ");
+    notebook1.setColorNotebook("Серебристый");
+    notebook1.setPrice(42990);
+
+    Notebook notebook2 = new Notebook();
+    notebook2.setModel("HIPER Workbook N1567");
+    notebook2.setId(4);
+    notebook2.setProductSeries("n1567rh5wi");
+    notebook2.setOperatingSystem("Windows 10 Professional");
+    notebook2.setRam("16 ГБ");
+    notebook2.setVolumeHdd("512 ГБ");
+    notebook2.setColorNotebook("Серебристый");
+    notebook2.setPrice(49990);
+
+    Notebook notebook3 = new Notebook();
+    notebook3.setModel("A-Data XPG XENIA 14");
+    notebook3.setId(2);
+    notebook3.setProductSeries("xenia14i5g11gxelx-bkcru");
+    notebook3.setOperatingSystem("Windows 10 Home");
+    notebook3.setRam("16 ГБ");
+    notebook3.setVolumeHdd("512 ГБ");
+    notebook3.setColorNotebook("Черный");
+    notebook3.setPrice(74990);
+
+    Notebook notebook4 = new Notebook();
+    notebook4.setModel("Huawei MateBook D 15");
+    notebook4.setId(3);
+    notebook4.setProductSeries("53013ert");
+    notebook4.setOperatingSystem("Windows 11 Home");
+    notebook4.setRam("8 ГБ");
+    notebook4.setVolumeHdd("256 ГБ");
+    notebook4.setColorNotebook("Серебристый");
+    notebook4.setPrice(44990);
+
+    Notebook notebook5 = new Notebook();
+    notebook5.setModel("MSI GF63 Thin 11UD-206XRU");
+    notebook5.setId(5);
+    notebook5.setProductSeries("9s7-16r612-206");
+    notebook5.setOperatingSystem("Free DOS");
+    notebook5.setRam("16 ГБ");
+    notebook5.setVolumeHdd("512 ГБ");
+    notebook5.setColorNotebook("Черный");
+    notebook5.setPrice(79990);
+
+    Set<Notebook> set = new HashSet<>();
+    set.add(notebook1);
+    set.add(notebook2);
+    set.add(notebook3);
+    set.add(notebook4);
+    set.add(notebook5);
+
+    for (Notebook n : set) {
+      System.out.println(n.toString());
+    }
+  }
 }
